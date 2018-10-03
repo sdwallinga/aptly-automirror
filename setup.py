f9e59d7042deaa8b1fe7a28bb1e9dfa11745fbf9
Sam Wallinga <samwallinga@gmail.com>
2018-10-03 12:04:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
         name='automirror',
         version='0.0.1',
-        description='5C AutoMirror',
+        description='aptly-automirror',
         author='Sam Wallinga',
-        author_email='sam@5thcolumn.net',
+        author_email='samwallinga@gmail.com',
         packages=find_packages(),
         install_requires=[
             'hvac',
